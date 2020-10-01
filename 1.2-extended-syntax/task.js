d307d48d54e77db4a81dc7cca146915d891d8f60
pfzw90 <pfzw90@gmail.com>
2020-10-01 21:44:56 +0300

    Homework 1.2 - extended syntax

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,57 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+        
+    "use strict";
+
+    let discr = b**2 - 4 * a * c;
+    let x = [];
+
+    if (discr > 0) {
+    	let x1 = (-b + discr**0.5)/(2 * a);
+    	let x2 = (-b - discr**0.5)/(2 * a);
+
+    	x.push(x1, x2);
+    }
+
+    else if (discr == 0) {
+    	x.push(-b / (2 * a));
+    }
+
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    
+    "use strict";
+
+    let len = marks.length;
+
+    if (len > 0) {
+
+    	if (len > 5) {
+    		console.log(`Оценок больше 5 (${len}). Среднее значение будет вычислено по первым 5 оценкам.`);
+    		marks.splice(len - 5);
+    		len = 5;
+    	}
+
+    	let total = 0;
+    	for (let mark of marks) {
+    		console.log(mark);
+    		total = total + mark;
+    	}
+
+    	return total/len;
+    } 
+
+	else return 0;
 }
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+       
+    "use strict";
+
+    let now = new Date();
+
+    if ((now.getFullYear() - dateOfBirthday.getFullYear()) >= 18) return `Не желаете ли олд-фэшн, ${name}?`;
+    else return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+
 }
