3a1df0a6a5c0d5896aa0f693563a8c03415505fc
pfzw90 <pfzw90@gmail.com>
2020-10-07 21:09:32 +0300

    Homework for 1.3 - Data Types

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,43 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    
+    
+    if (typeof parseInt(percent) !== "number") {
+    	return `“Параметр ПРОЦЕНТ содержит неправильное значение ${percent}”`;
+    }
+
+    else if (typeof parseInt(contribution) !== "number") {
+  		return `“Параметр ПЕРВОНАЧАЛЬНЫЙ ВЗНОС содержит неправильное значение ${contribution}”`;
+    }
+
+    else if (typeof parseInt(amount) !== "number") {
+  		return `“Параметр СУММА КРЕДИТА содержит неправильное значение ${amount}”`;
+    }
+
+    else {
+
+    	percent = Number(percent);
+    	contribution = Number(contribution);
+    	amount = Number(amount);
+    	console.log(percent, contribution, amount);
+    	let S = amount - contribution;
+    	let P = percent / 12 / 100;
+    	let n = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth());
+    	console.log(S,P,n, date.getFullYear(), new Date().getFullYear());
+    	let sum = (S * (P + P/(((1+P) ** n) - 1))) * n;
+    	console.log(sum)
+    	return Number(sum.toFixed(2));
+    }
+
+
+
+
+
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+	if (typeof name !== "string" || name.length < 1) name = 'Аноним';
+	return `Привет, мир! Меня зовут ${name}`
 }
