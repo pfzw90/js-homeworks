181ca7f8fb120ead5922c18268c95cc8db5edcc4
pfzw90 <pfzw90@gmail.com>
2020-10-15 21:26:52 +0300

    tasks 1 and 2 completed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,21 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict";
+
+String.prototype.isPalindrome = function() {
+    let string = this.toLowerCase().replace(/[^а-я]/g,"");
+    let string_reversed = string.split('').reverse().join('');
+    return (string == string_reversed);    
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let n = 0, average = 0;
+
+    for (let mark in marks) {
+        average = average + Number(marks[mark]);
+        n++;
+    }
+    
+    if (n) return average = Math.round(average/n);
+    return 0;
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
-}
