fe3555d291902772dd249a8f80130176a6d32fc1
Natalya Chekhonina <67958805+pfzw90@users.noreply.github.com>
2020-10-16 19:53:28 +0300

    -

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -2,8 +2,8 @@
 
 String.prototype.isPalindrome = function() {
     let string = this.toLowerCase().replace(/[^а-я]/g,"");
-    let string_reversed = string.split('').reverse().join('');
-    return (string == string_reversed);    
+    let stringReversed = string.split('').reverse().join('');
+    return (string == stringReversed);    
 }
 
 
@@ -21,23 +21,23 @@ function getAverageMark(marks) {
 
 function checkBirthday(birthday) {
     let now = new Date();
-    let user_birthday = new Date(birthday);
-    let now_unix = now.getTime();
-    let user_birthday_unix = user_birthday.getTime();
-    let diff = now_unix - user_birthday_unix;
+    let userBirthday = new Date(birthday);
+    let nowUnix = now.getTime();
+    let userBirthdayUnix = userBirthday.getTime();
+    let diff = nowUnix - userBirthdayUnix;
     
-    let leap_years = 0, years = 0;
-    for (let year = user_birthday.getFullYear(); year < now.getFullYear(); year++) {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) leap_years++;
+    let leapYears = 0, years = 0;
+    for (let year = userBirthday.getFullYear(); year < now.getFullYear(); year++) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) leapYears++;
         else years++;
     }
 
-    if (years + leap_years >= 18) return true;
+    if (years + leapYears >= 18) return true;
 
-    let total_ms_in_prev_years = (years * 365 + leap_years * 366) * 24 * 60 * 60 * 1000;
-    let total_ms_this_year_before_birthday = diff - total_ms_in_prev_years;
-    let total_ms_this_year_passed = now_unix - new Date(now.getFullYear(), 0, 1).getTime();
+    let totalMsInPrevYears = (years * 365 + leapYears * 366) * 24 * 60 * 60 * 1000;
+    let totalMsThisYearBeforeBirthday = diff - totalMsInPrevYears;
+    let totalMsThisYearPassed = nowUnix - new Date(now.getFullYear(), 0, 1).getTime();
 
-    return (total_ms_this_year_before_birthday >= total_ms_this_year_passed);
+    return (totalMsThisYearBeforeBirthday >= totalMsThisYearPassed);
    
 }
