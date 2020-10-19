e606e5209b680a6d2a6be7382129601bb72a35cc
pfzw90 <pfzw90@gmail.com>
2020-10-19 22:32:15 +0300

    Update task.js

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -85,7 +85,7 @@ class Library {
         let i = 0;
         for (let book of this.books) {
             if (book.name == bookName) {
-                this.books.splice(i);
+                this.books.splice(i, 1);
                 return book;             
             } 
             i++;
@@ -104,8 +104,66 @@ myHomeLibrary.addBook(theMartianCronicles);
 myHomeLibrary.addBook(toKillaMockingbird);
 myHomeLibrary.addBook(myInventions);
 myHomeLibrary.findBookBy('releaseDate', 1919);
-myHomeLibrary.giveBookByName('My Inventions');
-myInventions.state = 21;
-myInventions.fix();
-myHomeLibrary.addBook(myInventions);
+myHomeLibrary.giveBookByName('The Martian Chronicles');
+theMartianCronicles.state = 21;
+theMartianCronicles.fix();
+myHomeLibrary.addBook(theMartianCronicles);
 
+
+
+class StudentLog {
+    constructor(name) {
+        this.name = name;
+        this.grades = {};
+    }
+    
+    getName() {
+        return this.name;
+    }
+
+    #checkSubjExistance(subject) {
+        console.log(this.grades);
+        if (this.grades.hasOwnProperty(subject)) return true;
+        else {
+            console.log('предмет не найден');
+            return false;
+        }
+
+    }
+    
+    addGrade(grade, subject) {
+        let result = '';
+
+        if ((typeof grade !== 'number') && (!(0 < grade < 6))) {
+            result += `Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5. \n`;
+        }
+
+        else if (this.#checkSubjExistance(grade)) {
+            this.grades[subject].push(grade);
+        }
+        
+        else {
+            this.grades[subject] = [];
+            this.grades[subject].push(grade);
+        }
+
+        if (this.#checkSubjExistance(grade)) result += this.grades[subject].length;
+        else result += '0';
+
+        console.log(result);
+        return result;
+    }
+
+
+    getAverageBySubject(subject) {
+        let sum = 0;
+        if (this.#checkSubjExistance(subject)) {
+            for (let grade in this.grades[subject]) {
+                sum =+ grade;
+            }
+            return sum/this.grades[subject].length;
+            
+        }
+        return sum;
+    }
+}
