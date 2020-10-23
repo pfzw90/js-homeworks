79b295631b4c9abd0afcea84a153afeefa7eb047
Natalya Chekhonina <pfzw90@gmail.com>
2020-10-23 16:01:34 +0300

    1

---
+++ 3.2-array-proto-hocs/functions.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+
+const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+console.log(weapons);
+
+let getNames = () => weapons.map(weapon => weapon.name);
+let getCountReliableWeapons = (dur) => weapons.filter(weapon => weapon.durability > dur).length;
+let hasReliableWeapons = (rel) => weapons.some(weapon => weapon.initDurability > rel);
+let getReliableWeaponsNames = (rel) => weapons.filter(weapon => weapon.initDurability > rel).map(weapon => weapon.name);
+let getTotalDamage = () => weapons.reduce((totalDmg, weapon) => totalDmg + weapon.attack , 0);
