641784b1dd0bed9b21c09f6044aa10e11498ace6
pfzw90 <pfzw90@gmail.com>
2020-10-31 18:17:52 +0300

    kostylnyi varik

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -2,45 +2,92 @@
 
 class AlarmClock {
     constructor() {
-        this.AlarmCollection = [],
-        this.timerId 
+        this.alarmCollection = [];
+        this.timerId = null;
     }
 
-    addClock(start_time, action, id) {
+    addClock(startTime, action, id) {
+        console.log(this.alarmCollection);
         if (!id) throw new Error("ID будильника не задан");
-        if (this.AlarmCollection.some(alarm => alarm.id === id)) {
+        if (this.alarmCollection.some(alarm => alarm.id === id)) {
             console.error("Будильник с таким ID уже существует");
             return;
         }
-        this.AlarmCollection.push({"start_time" : start_time, "action" : action, "id" : id});
-        console.log(this.AlarmCollection[this.AlarmCollection.length() - 1]);       
+        this.alarmCollection.push({"startTime" : startTime, "action" : action, "id" : id});
+        console.log(this.alarmCollection.length);       
     }
 
     removeClock(id) {
-        return Boolean(this.AlarmCollection.splice(this.AlarmCollection.findIndex(alarm => alarm.id === id) , 1).length());     
+        return Boolean(this.alarmCollection.splice(this.alarmCollection.findIndex(alarm => alarm.id === id) , 1).length);     
     }
 
     getCurrentFormattedTime() {
         let now = new Date();
-        return `${now.getHours}:${now.getMinutes}`;
+        return `${now.getHours()}:${now.getMinutes()}`;
     }
 
     start() {
-        checkClock => (alarm) => { if (alarm.start_time === this.getCurrentFormattedTime()) alarm.action(); }
-        if (this.timerId === "undefined") {
-            this.timerId = setInterval(() => {this.AlarmCollection.map(checkClock(alarm))}, 1);
+        let checkClock = (alarm) => { if (alarm.startTime === this.getCurrentFormattedTime()) alarm.action(); }
+        if (this.timerId === null) {
+            this.timerId = setInterval(() => {this.alarmCollection.forEach(alarm => checkClock(alarm))}, 1);
         }
+    }
     
     stop() {
-        if (this.timerId !== "undefined") {
+        if (this.timerId !== null) {
             clearInterval(this.timerId);
-            this.timerId = void 0;
+            this.timerId = null;
         }
     }
 
+    printAlarms() {
+        this.alarmCollection.forEach(alarm => console.log(`ID: ${alarm.id}, TIME: ${alarm.startTime}`));
     }
 
-
+    clearAlarms() {
+        this.alarmCollection.splice(0, this.alarmCollection.length);
+        this.timerId = null;
+    }
 
 }
+
+function testCase() {
+    let form = (date) => `${date.getHours()}:${date.getMinutes()}`; 
+    let testAlarm = new AlarmClock;
+
+    let time = new Date();
+    testAlarm.addClock(form(time), ()=>{
+        testAlarm.alarmCollection[testAlarm.alarmCollection.findIndex((alarm) => alarm.id === '0')].startTime = null;
+        let i = 0;
+        let t = setInterval(()=>{
+            if (i >= 3) clearInterval(t);
+            else console.log('Test function running...' + (i++).toString()); 
+            }, 2000);
+    }, '0');
+
+    time.setMinutes(time.getMinutes() + 1);
+    testAlarm.addClock(form(time),()=>{
+        console.log(`Hello, I'm second alarm!`);
+        testAlarm.printAlarms();
+        testAlarm.removeClock('1');
+        testAlarm.printAlarms();
+        
+    }, '1');
+
+    time.setMinutes(time.getMinutes() + 1);
+    testAlarm.addClock(form(time),()=>{
+        console.log(`Hello, I'm third alarm!`);
+        testAlarm.stop();
+        testAlarm.clearAlarms();
+        testAlarm.printAlarms();
+    }, '2');
+
+    testAlarm.printAlarms();
+    testAlarm.start();
+}
+
+testCase();
+
+
+
 
